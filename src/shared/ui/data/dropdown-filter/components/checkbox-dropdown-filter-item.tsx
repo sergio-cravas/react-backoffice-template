a3bfebe1b40b647566e0b9609e55a25f76f04899
sergio-cravas <sergio.cravas@gmail.com>
2025-10-24 16:09:48 -0600

    refactor: improve inputProps handling in CheckboxDropdownFilterItem component

--- src/shared/ui/data/dropdown-filter/components/checkbox-dropdown-filter-item.tsx
+++ src/shared/ui/data/dropdown-filter/components/checkbox-dropdown-filter-item.tsx
@@ -7,7 +7,8 @@ import { CheckboxItem } from '../types/dropdown-filter.types';
 type Props = { control: Control<any, any>; item: CheckboxItem };
 
 function CheckboxDropdownFilterItem({ control, item }: Props) {
-  return <CheckboxField control={control} name={item.name} inputProps={{ ...item }} />;
+  const { type, value, ...inputProps } = item;
+  return <CheckboxField control={control} name={item.name} inputProps={inputProps} />;
 }
 
 export default CheckboxDropdownFilterItem;
