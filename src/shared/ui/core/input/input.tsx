edab67cd265e8b9cc85eadf18806209ad6320289
sergio-cravas <sergio.cravas@gmail.com>
2025-10-26 20:22:54 -0600

    feat: :sparkles: Add support for left element rendering in Input component

--- src/shared/ui/core/input/input.tsx
+++ src/shared/ui/core/input/input.tsx
@@ -17,6 +17,7 @@ export type InputProps = {
   invalid?: boolean;
   errorMessage?: string;
   className?: string;
+  leftElement?: React.ReactNode | string;
   rightElement?: React.ReactNode | string;
   style?: React.CSSProperties;
   onChange?: (value: string) => void;
@@ -33,6 +34,7 @@ export const Input = forwardRef(
       className,
       invalid,
       errorMessage,
+      leftElement,
       rightElement,
       style,
       type,
@@ -69,6 +71,12 @@ export const Input = forwardRef(
       [min, max, onChange]
     );
 
+    const renderLeftElement = () => {
+      if (!leftElement) return null;
+
+      return <div className="input-container__field__left-icon">{leftElement}</div>;
+    };
+
     const renderRightElement = () => {
       if (type === 'password' && !disabled && !props.readOnly) {
         return (
@@ -90,19 +98,17 @@ export const Input = forwardRef(
         {!!label && <Label htmlFor={name}>{required ? `${label} *` : label}</Label>}
 
         <div className="input-container__field">
+          {renderLeftElement()}
+
           <input
             {...props}
             ref={ref}
-            // className={cn('input-container__field__content', {
-            //   'input-container__field__content--invalid': invalid,
-            //   'input-container__field__content--disabled': disabled,
-            //   'input-container__field__content--number-input': type === 'number',
-            // })}
             data-slot="input"
             className={cn(
               'file:text-foreground placeholder:text-muted-foreground selection:bg-primary selection:text-primary-foreground dark:bg-input/30 border-input h-9 w-full min-w-0 rounded-md border bg-transparent px-3 py-1 text-base shadow-xs transition-[color,box-shadow] outline-none file:inline-flex file:h-7 file:border-0 file:bg-transparent file:text-sm file:font-medium disabled:pointer-events-none disabled:cursor-not-allowed disabled:opacity-50 md:text-sm',
               'focus-visible:border-ring focus-visible:ring-ring/50 focus-visible:ring-[3px]',
               'aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive',
+              leftElement && 'pl-10',
               className
             )}
             name={name}
