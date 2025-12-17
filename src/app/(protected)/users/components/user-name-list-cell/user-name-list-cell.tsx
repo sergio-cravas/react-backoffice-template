8ecffccf8f6336a9628a5b2662d803caa69feb68
sergio-cravas <sergio.cravas@gmail.com>
2025-12-17 17:28:57 +0100

    refactor: :recycle: Remove user-name-list-cell.scss and update UserNameListCell component to use Avatar for improved styling

--- src/app/(protected)/users/components/user-name-list-cell/user-name-list-cell.tsx
+++ src/app/(protected)/users/components/user-name-list-cell/user-name-list-cell.tsx
@@ -1,6 +1,5 @@
+import { Avatar, AvatarFallback, AvatarImage } from '@/shared/ui/core/avatar';
 import { Text } from '@/shared/ui/core/text';
-
-import './user-name-list-cell.scss';
 
 type UserNameListCellProps = {
   firstName: string;
@@ -8,11 +7,15 @@ type UserNameListCellProps = {
   imageUrl: string;
 };
 
-function UserNameListCell({ firstName, lastName, imageUrl }: UserNameListCellProps) {
+function UserNameListCell({ firstName = '', lastName = '', imageUrl = '' }: UserNameListCellProps) {
   return (
-    <div className="user-name-list-cell">
-      <img src={imageUrl} width={24} height={24} style={{ borderRadius: '100%', background: 'lightgray' }} />
-      <Text>{firstName + ' ' + lastName}</Text>
+    <div className="flex items-center gap-2">
+      <Avatar className="h-6 w-6">
+        <AvatarImage src={imageUrl} alt={firstName} />
+        <AvatarFallback>{firstName?.substring(0, 2)}</AvatarFallback>
+      </Avatar>
+
+      <Text as="span">{firstName + ' ' + lastName}</Text>
     </div>
   );
 }
